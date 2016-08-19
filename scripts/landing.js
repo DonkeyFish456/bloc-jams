19ff0b04a97ecade94e879a45ad6c8c03f606f4a
DonkeyFish456 <stancalderon@gmail.com>
2016-08-18 20:30:16 -0700

    add css transition, connect javascript

---
+++ scripts/landing.js
@@ -0,0 +1,14 @@
+var animatePoints = function () {
+	var points = document.getElementsByClassName('point');
+
+	var revealPoint = function(index){
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+	}
+	
+	for (var i in points){
+	revealPoint(i);
+	}
+};
