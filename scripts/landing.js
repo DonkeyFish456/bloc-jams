05a62d3b5407fea117c4d0c272b1b8edeb55ef42
DonkeyFish456 <stancalderon@gmail.com>
2016-08-20 13:09:34 -0700

    add scroll event

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,14 +1,24 @@
-var animatePoints = function () {
-	var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
-	var revealPoint = function(index){
-		points[index].style.opacity = 1;
-		points[index].style.transform = "scaleX(1) translateY(0)";
-		points[index].style.msTransform = "scaleX(1) translateY(0)";
-		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-	}
-	
-	for (var i in points){
-	revealPoint(i);
-	}
+var revealPoint = function(point){
+		point.style.opacity = 1;
+		point.style.transform = "scaleX(1) translateY(0)";
+		point.style.msTransform = "scaleX(1) translateY(0)";
+		point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
+
+var animatePoints = function(points) {
+	forEach(points, revealPoint);
+};
+
+window.onload = function() {
+	if (window.innerHeight > 950){animatePoints(pointsArray);}
+	var sellingPoints = document.getElementsByClassName('selling-points')[0];
+	var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+	
+	window.addEventListener('scroll', function(event){
+		if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+	});
+}
---
+++ scripts/utilities.js
@@ -0,0 +1,8 @@
+
+
+var forEach = function(array, callback){
+	for (var i in array){
+		callback(array[i]);
+	}
+}
+
